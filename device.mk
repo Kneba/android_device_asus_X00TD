7a02144ac1c81785cf7f495069e4047523354b63
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2022-02-13 10:56:31 +0530

    X00TD: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144
    Signed-off-by: dotkit <ewprjkt@proton.me>
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- device.mk
+++ device.mk
@@ -459,6 +459,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
