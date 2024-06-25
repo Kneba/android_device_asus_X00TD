5d364cdd4946b35d2e5d2851c065322df97a9b53
Nanhumly <admin@warbler.cc>
2024-06-25 11:14:20 +0000

    X00TD: Build android.hardware.camera.provider@2.6
    Change-Id: Ia8a551be6d20c3b843339f2d43542b890b195eef
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- device.mk
+++ device.mk
@@ -84,9 +84,9 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     camera.sdm660 \
