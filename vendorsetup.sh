87afd2f89d7eb25009b1877081bd7614497cc523
Kneba <abenkenary3@gmail.com>
2024-10-08 17:42:04 +0000

    X00TD: Track system_bpf from our fork
    Change-Id: I9adc0d7d6dcd64216433f8869d1307cfffb4fc78

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,10 @@ git clone --depth=1 https://github.com/texascake/android_kernel_asus_sdm660 -b 1
 #rm -rf kernel/asus/sdm660/KernelSU/userspace
 
 rm -rf device/asus/X00TD
-git clone --depth=1 https://github.com/Kneba/android_device_asus_X00TD device/asus/X00TD
+git clone --depth=1 https://github.com/Kneba/android_device_asus_X00TD -b 14 device/asus/X00TD
+
+rm -rf system/bpf
+git clone --depth=1 https://github.com/Kneba/android_system_bpf -b 14 system/bpf
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/Kneba/android_vendor_asus_X00TD -b 14 vendor/asus
